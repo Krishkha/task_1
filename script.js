c08d528fe04f3d8bac7de7f48b9d146cb4c49f02
Krishkha <khatikarkrish18@gmail.com>
2024-04-09 10:00:00 +0530

    new projet for sou

---
+++ script.js
@@ -0,0 +1,23 @@
+
+// *click function for top box to show and hide
+let topbox = document.getElementById('topbox');
+
+function topclickshow(){
+    topbox.style.display = "block";
+}
+
+function topclickhide(){
+    topbox.style.display = "none";
+}
+
+
+// *click function for bottom box to show and hide
+let bottombox = document.getElementById('bottombox');
+
+function bottomclickshow(){
+    bottombox.style.display = "block";
+}
+
+function bottomclickhide(){
+    bottombox.style.display = "none";
+}
