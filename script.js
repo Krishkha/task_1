e099d7c13256c373ca888dbe4e32945d5f340dc6
Krishkha <khatikarkrish18@gmail.com>
2024-04-11 10:12:50 +0530

    image chage function

--- script.js
+++ script.js
@@ -43,3 +43,87 @@ function rightclickshow(){
 function rightclickhide(){
     rightbox.style.display = "none";
 }
+
+
+// *click to change a image function
+
+let item1 = document.getElementById('item1');
+let item2 = document.getElementById('item2');
+let item3 = document.getElementById('item3');
+let item4 = document.getElementById('item4');
+let item5 = document.getElementById('item5');
+let img1 = document.getElementById('secton-4_img1');
+let img2 = document.getElementById('secton-4_img2');
+let img3 = document.getElementById('secton-4_img3');
+let img4 = document.getElementById('secton-4_img4');
+let img5 = document.getElementById('secton-4_img5');
+let txt1 = document.getElementById('txt1');
+let txt2 = document.getElementById('txt2');
+let txt3 = document.getElementById('txt3');
+let txt4 = document.getElementById('txt4');
+let txt5 = document.getElementById('txt5');
+
+
+item1.addEventListener("click",() => {
+    img1.style.display = 'block';
+    txt1.style.display = 'block';
+    img2.style.display = 'none';
+    txt2.style.display = 'none';
+    img3.style.display = 'none';
+    txt3.style.display = 'none';
+    img4.style.display = 'none';
+    txt4.style.display = 'none';
+    img5.style.display = 'none';
+    txt5.style.display = 'none';
+})
+
+item2.addEventListener("click",() => {
+    img1.style.display = 'none';
+    txt1.style.display = 'none';
+    img2.style.display = 'block';
+    txt2.style.display = 'block';
+    img3.style.display = 'none';
+    txt3.style.display = 'none';
+    img4.style.display = 'none';
+    txt4.style.display = 'none';
+    img5.style.display = 'none';
+    txt5.style.display = 'none';
+})
+
+item3.addEventListener("click",() => {
+    img1.style.display = 'none';
+    txt1.style.display = 'none';
+    img2.style.display = 'none';
+    txt2.style.display = 'none';
+    img3.style.display = 'block';
+    txt3.style.display = 'block';
+    img4.style.display = 'none';
+    txt4.style.display = 'none';
+    img5.style.display = 'none';
+    txt5.style.display = 'none';
+})
+
+item4.addEventListener("click",() => {
+    img1.style.display = 'none';
+    txt1.style.display = 'none';
+    img2.style.display = 'none';
+    txt2.style.display = 'none';
+    img3.style.display = 'none';
+    txt3.style.display = 'none';
+    img4.style.display = 'block';
+    txt4.style.display = 'block';
+    img5.style.display = 'none';
+    txt5.style.display = 'none';
+})
+item5.addEventListener("click",() => {
+    img1.style.display = 'none';
+    txt1.style.display = 'none';
+    img2.style.display = 'none';
+    txt2.style.display = 'none';
+    img3.style.display = 'none';
+    txt3.style.display = 'none';
+    img4.style.display = 'none';
+    txt4.style.display = 'none';
+    img5.style.display = 'block';
+    txt5.style.display = 'block';
+})
