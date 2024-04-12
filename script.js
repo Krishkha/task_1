e704e0f11da5ea56c813fd41acb64aaa72c0a6b1
Krishkha <khatikarkrish18@gmail.com>
2024-04-12 09:23:23 +0530

    develop new container(container 3)

--- script.js
+++ script.js
@@ -62,68 +62,99 @@ let txt2 = document.getElementById('txt2');
 let txt3 = document.getElementById('txt3');
 let txt4 = document.getElementById('txt4');
 let txt5 = document.getElementById('txt5');
+let pera1 = document.getElementById('pera1');
+let pera2 = document.getElementById('pera2');
+let pera3 = document.getElementById('pera3');
+let pera4 = document.getElementById('pera4');
+let pera5 = document.getElementById('pera5');
+
 
 
 item1.addEventListener("click",() => {
     img1.style.display = 'block';
     txt1.style.display = 'block';
+    pera1.style.display = 'block';
     img2.style.display = 'none';
     txt2.style.display = 'none';
+    pera2.style.display = 'none';
     img3.style.display = 'none';
     txt3.style.display = 'none';
+    pera3.style.display = 'none';
     img4.style.display = 'none';
     txt4.style.display = 'none';
+    pera4.style.display = 'none';
     img5.style.display = 'none';
     txt5.style.display = 'none';
+    pera5.style.display = 'none';
 })
 
 item2.addEventListener("click",() => {
     img1.style.display = 'none';
     txt1.style.display = 'none';
+    pera1.style.display = 'none';
     img2.style.display = 'block';
     txt2.style.display = 'block';
+    pera2.style.display = 'block';
     img3.style.display = 'none';
     txt3.style.display = 'none';
+    pera3.style.display = 'none';
     img4.style.display = 'none';
     txt4.style.display = 'none';
+    pera4.style.display = 'none';
     img5.style.display = 'none';
     txt5.style.display = 'none';
+    pera5.style.display = 'none';
 })
 
 item3.addEventListener("click",() => {
     img1.style.display = 'none';
     txt1.style.display = 'none';
+    pera1.style.display = 'none';
     img2.style.display = 'none';
     txt2.style.display = 'none';
+    pera2.style.display = 'none';
     img3.style.display = 'block';
     txt3.style.display = 'block';
+    pera3.style.display = 'block';
     img4.style.display = 'none';
     txt4.style.display = 'none';
+    pera4.style.display = 'none';
     img5.style.display = 'none';
     txt5.style.display = 'none';
+    pera5.style.display = 'none';
 })
 
 item4.addEventListener("click",() => {
     img1.style.display = 'none';
     txt1.style.display = 'none';
+    pera1.style.display = 'none';
     img2.style.display = 'none';
     txt2.style.display = 'none';
+    pera2.style.display = 'none';
     img3.style.display = 'none';
     txt3.style.display = 'none';
+    pera3.style.display = 'none';
     img4.style.display = 'block';
     txt4.style.display = 'block';
+    pera4.style.display = 'block';
     img5.style.display = 'none';
     txt5.style.display = 'none';
+    pera5.style.display = 'none';
 })
 item5.addEventListener("click",() => {
     img1.style.display = 'none';
     txt1.style.display = 'none';
+    pera1.style.display = 'none';
     img2.style.display = 'none';
     txt2.style.display = 'none';
+    pera2.style.display = 'none';
     img3.style.display = 'none';
     txt3.style.display = 'none';
+    pera3.style.display = 'none';
     img4.style.display = 'none';
     txt4.style.display = 'none';
+    pera4.style.display = 'none';
     img5.style.display = 'block';
     txt5.style.display = 'block';
+    pera5.style.display = 'block';
 })
