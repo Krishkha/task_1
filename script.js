89660bb3c744da6f27ab7441cee2825695e1f0cc
KK <khatikarkrishk@gmail.com>
2024-04-18 09:58:11 +0530

    dotted scroll and connect btn

--- script.js
+++ script.js
@@ -42,7 +42,10 @@ function rightclickhide() {
   rightbox.style.display = "none";
 }
 
-// *click to change a image function
+
+/* ==========================================================
+container 2
+========================================================== */
 
 let items = [
   document.getElementById('item1'),
@@ -114,116 +117,10 @@ items.forEach((item,index) => {
     });
 })
 
-// let item1 = document.getElementById('item1');
-// let item2 = document.getElementById('item2');
-// let item3 = document.getElementById('item3');
-// let item4 = document.getElementById('item4');
-// let item5 = document.getElementById('item5');
-// let img1 = document.getElementById('secton-4_img1');
-// let img2 = document.getElementById('secton-4_img2');
-// let img3 = document.getElementById('secton-4_img3');
-// let img4 = document.getElementById('secton-4_img4');
-// let img5 = document.getElementById('secton-4_img5');
-// let txt1 = document.getElementById('txt1');
-// let txt2 = document.getElementById('txt2');
-// let txt3 = document.getElementById('txt3');
-// let txt4 = document.getElementById('txt4');
-// let txt5 = document.getElementById('txt5');
-// let pera1 = document.getElementById('pera1');
-// let pera2 = document.getElementById('pera2');
-// let pera3 = document.getElementById('pera3');
-// let pera4 = document.getElementById('pera4');
-// let pera5 = document.getElementById('pera5');
 
-// item1.addEventListener("click",() => {
-//     img1.style.display = 'block';
-//     txt1.style.display = 'block';
-//     pera1.style.display = 'block';
-//     img2.style.display = 'none';
-//     txt2.style.display = 'none';
-//     pera2.style.display = 'none';
-//     img3.style.display = 'none';
-//     txt3.style.display = 'none';
-//     pera3.style.display = 'none';
-//     img4.style.display = 'none';
-//     txt4.style.display = 'none';
-//     pera4.style.display = 'none';
-//     img5.style.display = 'none';
-//     txt5.style.display = 'none';
-//     pera5.style.display = 'none';
-// })
-
-// item2.addEventListener("click",() => {
-//     img1.style.display = 'none';
-//     txt1.style.display = 'none';
-//     pera1.style.display = 'none';
-//     img2.style.display = 'block';
-//     txt2.style.display = 'block';
-//     pera2.style.display = 'block';
-//     img3.style.display = 'none';
-//     txt3.style.display = 'none';
-//     pera3.style.display = 'none';
-//     img4.style.display = 'none';
-//     txt4.style.display = 'none';
-//     pera4.style.display = 'none';
-//     img5.style.display = 'none';
-//     txt5.style.display = 'none';
-//     pera5.style.display = 'none';
-// })
-
-// item3.addEventListener("click",() => {
-//     img1.style.display = 'none';
-//     txt1.style.display = 'none';
-//     pera1.style.display = 'none';
-//     img2.style.display = 'none';
-//     txt2.style.display = 'none';
-//     pera2.style.display = 'none';
-//     img3.style.display = 'block';
-//     txt3.style.display = 'block';
-//     pera3.style.display = 'block';
-//     img4.style.display = 'none';
-//     txt4.style.display = 'none';
-//     pera4.style.display = 'none';
-//     img5.style.display = 'none';
-//     txt5.style.display = 'none';
-//     pera5.style.display = 'none';
-// })
-
-// item4.addEventListener("click",() => {
-//     img1.style.display = 'none';
-//     txt1.style.display = 'none';
-//     pera1.style.display = 'none';
-//     img2.style.display = 'none';
-//     txt2.style.display = 'none';
-//     pera2.style.display = 'none';
-//     img3.style.display = 'none';
-//     txt3.style.display = 'none';
-//     pera3.style.display = 'none';
-//     img4.style.display = 'block';
-//     txt4.style.display = 'block';
-//     pera4.style.display = 'block';
-//     img5.style.display = 'none';
-//     txt5.style.display = 'none';
-//     pera5.style.display = 'none';
-// })
-// item5.addEventListener("click",() => {
-//     img1.style.display = 'none';
-//     txt1.style.display = 'none';
-//     pera1.style.display = 'none';
-//     img2.style.display = 'none';
-//     txt2.style.display = 'none';
-//     pera2.style.display = 'none';
-//     img3.style.display = 'none';
-//     txt3.style.display = 'none';
-//     pera3.style.display = 'none';
-//     img4.style.display = 'none';
-//     txt4.style.display = 'none';
-//     pera4.style.display = 'none';
-//     img5.style.display = 'block';
-//     txt5.style.display = 'block';
-//     pera5.style.display = 'block';
-// })
-
+/* ==========================================================
+Slider
+========================================================== */
 document.addEventListener("DOMContentLoaded", function () {
   const carousel = document.querySelector(".carousel");
   const arrowBtns = document.querySelectorAll(".wrapper i");
